f516df712f5bcb69298af2fb508d9847229445d4
Sksham Kaushal <skshamkaushal@gmail.com>
2025-06-24 21:08:41 +0530

    first commit

--- src/Components/sections/Projects.jsx
+++ src/Components/sections/Projects.jsx
@@ -14,13 +14,12 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-              <h3 className="text-xl font-bold mb-2"> Cloud Platform</h3>
+              <h3 className="text-xl font-bold mb-2"> OPEN BOARD</h3>
               <p className="text-gray-400 mb-4">
-                Scalable cloud infrastructure management with real-time
-                monitoring and automated scaling.
+                A REAL TIME OPEN BOARD SITE 
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
+                {["HTML", "CSS", "JS", "socket.io","Express.js"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -33,11 +32,17 @@ export const Projects = () => {
               </div>
 
               <div className="flex justify-between items-center">
-                <a
-                  href="#"
+                  <a
+                  href="https://github.com/SKSHAMKAUSHAL/OPEN-BOARD.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
+                </a>
+                <a
+                  href="https://open-board-1.onrender.com/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Live link 👁
                 </a>
               </div>
             </div>
@@ -49,13 +54,12 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">AI Analytics Dashboard</h3>
+              <h3 className="text-xl font-bold mb-2">CAMERA-GALLERY-PROJECT</h3>
               <p className="text-gray-400 mb-4">
-                ML-powered data visualization platform with predictive analytics
-                and interactive reports.
+              A Camera Gallery which work i like a real camera app
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "TensorFlow", "D3.js", "Flask"].map((tech, key) => (
+                {["HTML", "CSS", "Javascript" , "Canvas","indexDB"].map((tech, key) => (
                   <span
                     key={key}
                     className="
@@ -71,13 +75,18 @@ export const Projects = () => {
                 ))}
               </div>
               <div className="flex justify-between items-center">
-                <a
-                  href="#"
+                   <a
+                  href="https://github.com/SKSHAMKAUSHAL/CAMERA-GALLERY-PROJECT.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
                 </a>
-                
+                <a
+                  href=" camera-gallery-sk.netlify.app"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Live link 👁
+                </a>
                 
               </div>
               
@@ -93,11 +102,11 @@ export const Projects = () => {
             >
               <h3 className="text-xl font-bold mb-2">E-Commerce Web App</h3>
               <p className="text-gray-400 mb-4">
-                Full-stack e-commerce with modern UI, secure payment
-                integration, and customizable product inventory.
+                e-commerce website with modern UI using firebase, payment
+                integration.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "TypeScript", "Stripe", "PostgreSQL"].map(
+                {["firebase", "TailwindCSS", "React", "razorpay"].map(
                   (tech) => (
                     <span
                       key={tech}
@@ -115,12 +124,20 @@ export const Projects = () => {
                 )}
               </div>
               <div className="flex justify-between items-center">
-                <a
-                  href="#"
+                
+                   <a
+                  href="https://github.com/SKSHAMKAUSHAL/E_SHOP_UP.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
                 </a>
+                <a
+                  href="https://shop-with-shopup.netlify.app/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Live link 👁
+                </a>
+
               </div>
             </div>
 
@@ -132,13 +149,11 @@ export const Projects = () => {
               transition-all
             "
             >
-              <h3 className="text-xl font-bold mb-2">Real-Time Chat App</h3>
+              <h3 className="text-xl font-bold mb-2">Movie App</h3>
               <p className="text-gray-400 mb-4">
-                Scalable chat platform supporting real-time messaging, presence,
-                and group chat features.
-              </p>
+              A sleek and responsive movie browsing application built using React. </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Socket.IO", "Express", "React", "Redis"].map((tech, key) => (
+                {["TMDB", "Netlify", "React"].map((tech, key) => (
                   <span
                     key={key}
                     className="
@@ -154,11 +169,17 @@ export const Projects = () => {
                 ))}
               </div>
               <div className="flex justify-between items-center ">
-                <a
-                  href="#"
+                  <a
+                  href="https://github.com/SKSHAMKAUSHAL/Movie-APP.git"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
+                </a>
+                <a
+                  href="https://skmovieapp.netlify.app/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  Live link 👁
                 </a>
               </div>
             </div>
